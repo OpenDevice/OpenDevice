7ec4e424ed871cc98a6f536112138f206f5eeccf
ricardo.jl.rufino@gmail.com <ricardo.jl.rufino@gmail.com>
2016-05-13 19:37:22 -0300

    update startup script

--- opendevice-middleware/server.sh
+++ opendevice-middleware/server.sh
@@ -6,7 +6,7 @@ cd "$( cd "$( dirname "${0}" )" && pwd )"
 
 case $1 in
 start)
-java -cp "opendevice-middleware.jar:lib/*" -Dapp.port=8181 -Dapp.dir=$WEB_PATH -Dapp.mode=local  br.com.criativasoft.opendevice.middleware.Main
+java -cp "opendevice-middleware.jar:lib/*" -Dconfig="config.js" -Dapp.port=8181 -Dapp.dir=$WEB_PATH -Dapp.mode=local  br.com.criativasoft.opendevice.middleware.Main
 ;;
 debug)
 java -Dapp.port=8181 -Dapp.dir=web -Dapp.mode=remote -Xdebug -Xrunjdwp:transport=dt_socket,address=8998,server=y -cp "lib/*" -jar opendevice-middleware.jar

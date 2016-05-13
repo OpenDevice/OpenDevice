c06ba19ccf79f38d52aeb67287bff0a1e916ad04
ricardo.jl.rufino@gmail.com <ricardo.jl.rufino@gmail.com>
2016-05-12 21:59:38 -0300

    pom.xml update

---
+++ opendevice-tests/src/main/java/opendevice/io/tests/util/ResetableTimer.java
@@ -0,0 +1,48 @@
+/*
+ * *****************************************************************************
+ * Copyright (c) 2013-2014 CriativaSoft (www.criativasoft.com.br)
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *  Ricardo JL Rufino - Initial API and Implementation
+ * *****************************************************************************
+ */
+
+package opendevice.io.tests.util;
+
+public class ResetableTimer extends Timer
+{
+    private Runnable  task;
+    private TimerTask timerTask;
+
+    public void schedule(Runnable runnable, long delay)
+    {
+        task = runnable;
+        timerTask = new TimerTask()
+        {
+            @Override
+            public void run()
+            {
+                task.run();
+            }
+        };
+        this.schedule(timerTask, delay);
+    }
+
+    public void reschedule(long delay)
+    {
+        timerTask.cancel();
+        timerTask = new TimerTask()
+        {
+            @Override
+            public void run()
+            {
+                task.run();
+            }
+        };
+        this.schedule(timerTask, delay);
+    }
+}

bf3712604e76ad768008ba1abd8ac89bfb813fb5
ricardo.jl.rufino@gmail.com <ricardo.jl.rufino@gmail.com>
2016-11-23 22:34:10 -0300

    core - configure using ODev.con file Instead of javascript

--- opendevice-core/src/main/java/br/com/criativasoft/opendevice/core/model/OpenDeviceConfig.java
+++ opendevice-core/src/main/java/br/com/criativasoft/opendevice/core/model/OpenDeviceConfig.java
@@ -15,6 +15,17 @@
 
 package br.com.criativasoft.opendevice.core.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+
 /**
  * OpenDevice configuration.
  *
@@ -22,40 +33,106 @@ package br.com.criativasoft.opendevice.core.model;
  */
 public class OpenDeviceConfig {
 
+    private static final Logger log  = LoggerFactory.getLogger(OpenDeviceConfig.class);
+
     public static final String LOCAL_APP_ID = "*";
 
     public static final int DEFAULT_PORT = 8181;
 
     private static OpenDeviceConfig INSTANCE;
 
-    private int port = DEFAULT_PORT;
+    private Properties properties;
 
-    private boolean tenantsEnabled = false;
+    private Boolean autoRegisterLocalDevice; // as attribute for fast access.
 
-    private boolean broadcastInputs = true;
 
-    private boolean databaseEnabled = false;
+    enum ConfigKey{
+        web_port("web.port", null, DEFAULT_PORT),
+        log_config("log.config", null, "logback-dev.xml"),
+        web_external_resources("web.external_resources", null, new ArrayList<String>()),
+        startup_script("startup_script", null, null),
+        tenants_enabled("tenants.enabled", null, false),
+        broadcast_inputs("internal.broadcastInputs", null, true),
+        database_enabled("database.enabled", null, false),
+        database_path("database.path", null, null),
+        database_engine("database.engine", null, null),
+        mqtt_enabled("mqtt.enabled", null, false),
+        auth_enabled("auth.enabled", null, false),
+        internal_autoregister("internal.autoRegisterLocalDevice", null, true),
+        ssl_certificateFile("ssl.certificateFile", null, null),
+        ssl_certificateKey("ssl.certificateKey", null, null),
+        ssl_certificatePass("ssl.certificatePass", null, null),
+        ;
 
-    private String databasePath = "/data";
+        private String key, description;
+        private Object defaultValue;
+        ConfigKey(String key, String descriptions, Object defaultValue){
+            this.key = "odev."+key;
+            this.description = descriptions;
+            this.defaultValue = defaultValue;
+        }
 
-    private boolean mqttEnabled = true;
+        public String getKey() {return key;}
 
-    private boolean authRequired = false;
+        public String getDescription() {return description;}
 
-    /**
-     * This allow local application registar devices without call addDevice
-     * Note: This is a experimental feature and may slow down device initialization
-     * @see br.com.criativasoft.opendevice.core.LocalDeviceManager#autoRegisterDevice(Device)
-     */
-    private boolean autoRegisterLocalDevice = true;
+        public Object getDefaultValue() {
+            return defaultValue;
+        }
+    }
 
-    private String certificateFile;
-
-    private String certificateKey;
-
-    private String certificatePass;
 
     private OpenDeviceConfig(){
+    }
+
+    public static OpenDeviceConfig get(){
+        if(INSTANCE == null){
+            INSTANCE = new OpenDeviceConfig();
+
+            String configDir = getConfigDirectory();
+
+            File config = new File(configDir + File.separator + "odev.conf");
+            if(config.exists()){
+                try {
+                    Properties properties = new Properties();
+                    properties.load(new FileInputStream(config));
+                    INSTANCE.properties = properties;
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }else{
+                INSTANCE.properties = new Properties();
+                log.info("Using default config");
+            }
+
+
+        }
+        return INSTANCE;
+    }
+
+    /**
+     * Get config directory from system properties or current directory
+     * @return
+     */
+    public static String getConfigDirectory(){
+        String configDir = System.getProperty("config-dir");
+        if(configDir == null) configDir = getHomeDirectory()  + File.separator + "conf";
+        return configDir;
+    }
+
+    /**
+     * Get data directory from system properties or current directory
+     * @return
+     */
+    public static String getDataDirectory(){
+        return getHomeDirectory() + File.separator + "data";
+    }
+
+    public static String getHomeDirectory(){
+        String dir = System.getProperty("odev-home");
+        if(dir == null) dir = System.getProperty("user.dir");
+        return dir;
     }
 
     /**
@@ -63,106 +140,209 @@ public class OpenDeviceConfig {
      * @return if multi-tenant is active
      */
     public boolean isTenantsEnabled() {
-        return tenantsEnabled;
+        return getBoolean(ConfigKey.tenants_enabled);
     }
 
-    public void setTenantsEnabled(boolean tenantsEnabled) {
-        this.tenantsEnabled = tenantsEnabled;
+    public void setTenantsEnabled(boolean value) {
+        set(ConfigKey.tenants_enabled, value);
     }
 
-    public static OpenDeviceConfig get(){
-        if(INSTANCE == null){
-            INSTANCE = new OpenDeviceConfig();
-        }
-
-        return INSTANCE;
-    }
 
     /**
      * Sets whether to make the broadcast of a command {@link br.com.criativasoft.opendevice.core.command.DeviceCommand} received by an incoming connection to the other incoming connections
-     * @param broadcastInputs (Default = true)
+     * @param value (Default = true)
      */
-    public void setBroadcastInputs(boolean broadcastInputs) {
-        this.broadcastInputs = broadcastInputs;
+    public void setBroadcastInputs(boolean value) {
+        set(ConfigKey.broadcast_inputs, value);
     }
 
-    public boolean isBroadcastInputs() {
-        return broadcastInputs;
-    }
+    public boolean isBroadcastInputs() {return getBoolean(ConfigKey.broadcast_inputs);}
 
-    public void setDatabaseEnabled(boolean databaseEnabled) {
-        this.databaseEnabled = databaseEnabled;
+    public void setDatabaseEnabled(boolean value) {
+        set(ConfigKey.database_enabled, value);
     }
 
     public boolean isDatabaseEnabled() {
-        return databaseEnabled;
+        return getBoolean(ConfigKey.database_enabled);
     }
 
     /**
      * Set certificate file to enable SSL support over (MQTT, HTTP, REST, WebSocket)
      */
-    public void setCertificateFile(String certificateFile) {
-        this.certificateFile = certificateFile;
+    public void setCertificateFile(String value) {
+        set(ConfigKey.ssl_certificateFile, value);
     }
 
-    public void setCertificateKey(String certificateKey) {
-        this.certificateKey = certificateKey;
+    public void setCertificateKey(String value) {
+        set(ConfigKey.ssl_certificateKey, value);
     }
 
-    public void setCertificatePass(String certificatePass) {
-        this.certificatePass = certificatePass;
+    public void setCertificatePass(String value) {
+        set(ConfigKey.ssl_certificatePass, value);
     }
 
     public String getCertificateFile() {
-        return certificateFile;
+        return getPath(ConfigKey.ssl_certificateFile);
     }
 
     public String getCertificateKey() {
-        return certificateKey;
+        return getString(ConfigKey.ssl_certificateKey);
     }
 
-    public String getCertificatePass() {
-        return certificatePass;
-    }
+    public String getCertificatePass() { return getString(ConfigKey.ssl_certificatePass); }
 
-    public void setPort(int port) {
-        this.port = port;
+    public void setPort(int value) {
+        set(ConfigKey.web_port, value);
     }
 
     public int getPort() {
-        return port;
+        return getInt(ConfigKey.web_port);
     }
 
-    public void setAuthRequired(boolean authRequired) {
-        this.authRequired = authRequired;
+    public void setAuthRequired(boolean value) {
+        set(ConfigKey.auth_enabled, value);
     }
 
     public boolean isAuthRequired() {
-        return authRequired;
+        return getBoolean(ConfigKey.auth_enabled);
     }
 
-    public void setMqttEnabled(boolean mqttEnabled) {
-        this.mqttEnabled = mqttEnabled;
+    public void setMqttEnabled(boolean value) {
+        set(ConfigKey.mqtt_enabled, value);
     }
 
     public boolean isMqttEnabled() {
-        return mqttEnabled;
+        return getBoolean(ConfigKey.mqtt_enabled);
     }
 
-    public void setDatabasePath(String databasePath) {
-        this.databasePath = databasePath;
-    }
+    public void setDatabasePath(String value) {set(ConfigKey.database_path, value); }
 
     public String getDatabasePath() {
-        return databasePath;
+        return getPath(ConfigKey.database_path);
     }
 
+    public void setDatabaseEngine(String value) {set(ConfigKey.database_path, value);}
 
-    public void setAutoRegisterLocalDevice(boolean autoRegisterLocalDevice) {
-        this.autoRegisterLocalDevice = autoRegisterLocalDevice;
+    public String getDatabaseEngine() { return getString(ConfigKey.database_engine);}
+
+
+    /**
+     * @see #isAutoRegisterLocalDevice()
+     */
+    public void setAutoRegisterLocalDevice(boolean value) {
+        this.autoRegisterLocalDevice = value;
+        set(ConfigKey.internal_autoregister, value);
     }
 
+    /**
+     * This allow local application registar devices without call addDevice
+     * Note: This is a experimental feature and may slow down device initialization
+     * @see br.com.criativasoft.opendevice.core.LocalDeviceManager#autoRegisterDevice(Device)
+     */
     public boolean isAutoRegisterLocalDevice() {
+        if(this.autoRegisterLocalDevice == null ) autoRegisterLocalDevice = getBoolean(ConfigKey.internal_autoregister);
         return autoRegisterLocalDevice;
     }
+
+    public List<String> getExternalResources() { return getList(ConfigKey.web_external_resources);}
+
+    public String getStartupScript() { return getString(ConfigKey.startup_script);}
+
+    public String getLogConfig() { return getPath(ConfigKey.log_config);}
+
+    //
+    // Properties Utils
+    //
+
+    private String get(ConfigKey config) {
+
+        if (properties.containsKey(config.getKey())) {
+            return properties.getProperty(config.getKey());
+        }
+
+        return null;
+    }
+
+    private void set(ConfigKey config, Object value) {
+
+        properties.setProperty(config.getKey(), value.toString());
+    }
+
+
+    public String getString(ConfigKey key) {
+
+        return get(key);
+    }
+
+    public int getInt(ConfigKey key) {
+
+        String v = get(key);
+
+        if (v == null) return (Integer) key.getDefaultValue();
+
+        return Integer.parseInt(v);
+    }
+
+    public boolean getBoolean(ConfigKey key) {
+
+        String v = get(key);
+
+        if (v == null) return (Boolean) key.getDefaultValue();
+
+        return Boolean.parseBoolean(v);
+    }
+
+    public List<String> getList(ConfigKey key) {
+
+        String v = get(key);
+
+        if (v == null) return (List) key.getDefaultValue();
+
+        String[] temp = v.split("\\s*,\\s*");
+
+        return new ArrayList<String>(Arrays.asList(temp));
+    }
+
+    public String getPath(ConfigKey key) {
+        String s = getString(key);
+        if (File.separatorChar == '/') {
+            return s;
+        }
+        return s.replaceAll("\\/", File.separator);
+    }
+
+    public Class<? extends Object> getClass(ConfigKey key) {
+        String className = getString(key);
+        try {
+            return Class.forName(className);
+        } catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String[] makeArray(String v) {
+
+        if (v.length() == 0) return new String[0];
+
+        String[] array = v.split("\\s*,\\s*");
+
+        for(int i = 0; i < array.length; i++) {
+            String s = array[i];
+            if ((s.startsWith("\"") && s.endsWith("\"")) || (s.startsWith("'") && s.endsWith("'"))) {
+                array[i] = s.substring(1, s.length() - 1);
+            }
+        }
+        return array;
+    }
+
+    public String[] getArray(ConfigKey key) {
+        String v = get(key).trim();
+
+        if (!v.startsWith("[") || !v.endsWith("]")) throw new IllegalArgumentException("Not an array: " + v);
+
+        v = v.substring(1, v.length() - 1).trim();
+
+        return makeArray(v);
+    }
+
 }

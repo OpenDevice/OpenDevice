00f113de8e1ef913f7a80d2a60f15ff5b050ccbb
ricardo.jl.rufino@gmail.com <ricardo.jl.rufino@gmail.com>
2018-06-26 00:39:10 -0300

    fix: deploy only master

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+  echo "<settings><servers><server><id>ossrh</id><username>\${env.OSSRH_USER}</username><password>\${env.OSSRH_PASS}</password></server></servers></settings>" > ~/settings.xml
+  mvn deploy --settings ~/settings.xml
+fi
+
